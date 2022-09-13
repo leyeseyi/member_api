16094c4285d71dcf096572959152b98bac2cec80
Oluseyi Olaleye <ictoluseyi@gmail.com>
2022-09-13 11:21:03 +0100

    implemented get member route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, request
+from flask import Flask, g, request, jsonify
 from database import get_db
 
 app = Flask(__name__)
@@ -10,6 +10,39 @@ def close_db(error):
 
 @app.route('/members', methods=['GET'])
 def get_members():
+    db = get_db()
+
+    members_cur = db.execute('select * from members')
+    members_result = members_cur.fetchall()
+
+    return_values = []
+    for member in members_result:
+        members_dict = {}
+
+        members_dict['id'] = member['id']
+        members_dict['name'] = member['name']
+        members_dict['email'] = member['email']
+        members_dict['level'] = member['level']
+        
+        return_values.append(members_dict)
+
+    return jsonify({'members': return_values})
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    db = get_db()
+
+    member_cur = db.execute('select * from members where id = ?', [member_id])
+    member = member_cur.fetchone()
+
+    return jsonify({
+        'id' : member['id'],
+        'name': member['name'],
+        'email': member['email'],
+        'level': member['level']
+    })
+
+@app.route('/members', methods=['POST'])
+def add_member():
     new_member_data =request.get_json()
     
     name = new_member_data['name']
@@ -20,16 +53,17 @@ def get_members():
 
     db.execute('insert into members (name, email, level) values (?,?,?)',[name, email, level])
     db.commit()
-    return 'The name is {}, email is {}, level is {}'.format(name,email, level)
 
-@app.route('/members/<int:member_id>', methods=['GET'])
-def get_member(member_id):
-    return 'This returns one member by ID'
+    member_cur = db.execute('select * from members where name = ?',[name])
+    new_member = member_cur.fetchone()
 
-@app.route('/members', methods=['POST'])
-def add_member():
+    return jsonify({
+        'id' : new_member['id'],
+        'name': new_member['name'],
+        'email': new_member['email'],
+        'level': new_member['level']
+    })
 
-    return 'This adds a new member.'
 
 @app.route('/members/<int:member_id>', methods=['PUT', 'PATCH'])
 def edit_member(member_id):
